4f1cc7ece808a4f35233dabbe12c87956b0cba34
AlexDev2808 <colores.2808.8000@gmail.com>
2023-09-08 14:47:38 -0600

    Iniciando proyecto devstagram y comenzando a crear las primeras vistas

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        
+    </head>
+    <body>
+
+        <nav>
+            <a href="#">Nosotros</a>
+            <a href="">Contacto</a>
+        </nav>
+
+        <h1>@yield('titulo')</h1>
+
+    </body>
+
+</html>
